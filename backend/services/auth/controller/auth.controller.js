e50867f4f5196be09752e5d7fac03035da2fab7f
Gagandeep Singh <deep8686385@gmail.com>
2026-09-16 14:57:23 +0530

    Update auth.controller.js

--- backend/services/auth/controller/auth.controller.js
+++ backend/services/auth/controller/auth.controller.js
@@ -48,8 +48,8 @@ export const login = async (req, res) => {
 
     res.cookie("session", sessionId, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/"
 });
